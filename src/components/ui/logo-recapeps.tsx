527ba1fcb5909e7ae50d5d25217ed56e7a2443fb
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-03-22 16:24:19 +0100

    fix: hopefully fix in the Module not found: Can't resolve '@/components/ui/provider'

--- src/components/ui/logo-recapeps.tsx
+++ src/components/ui/logo-recapeps.tsx
@@ -1,5 +1,4 @@
 import styles from './logo.module.css';
-styles['cls-5']
 export default function Logo() {
     return (
         <svg width="220px" height="220px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 585.65 594.86">
