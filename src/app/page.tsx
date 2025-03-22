657003fba94ad2502a2be6d8cf84395a9e92a622
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-03-22 16:18:20 +0100

    feat: update layout with Chakra UI components and remove unused styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,120 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import {
+  Box,
+  Button,
+  Heading,
+  Text,
+  SimpleGrid,
+  VStack,
+  Image,
+  Card,
+  Stack,
+  Icon,
+  Highlight,
+  LinkBox,
+  LinkOverlay
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { LuNotebookPen, LuListTodo } from "react-icons/lu";
+import { FiBookOpen } from "react-icons/fi"
+import Logo from "@/components/ui/logo-recapeps";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <Box >
+      <VStack alignItems="center" width="100%" textAlign="center">
+        <Logo />
+        <Heading as="h1" size="5xl" letterSpacing="tight">
+          <Highlight query="RECAP'EPS" styles={{ color: "orange.600" }}>
+            Bienvenue sur RECAP'EPS
+          </Highlight>
+        </Heading>
+        <Heading size={{ base: "xl", md: "2xl" }} as="h2">
+          Réussir ce n'est qu'une question d'entraînement, avec Recap'eps, s'entraîner n'a jamais été aussi facile 😎
+        </Heading>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          align="center"
+        >
+          <Button
+            rounded="full"
+            asChild
+            size="2xl"
+            bg="orange.500"
+            color="white"
+            _hover={{
+              bg: "orange.600",
+              transform: "scale(1.1)"
+            }}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <Link href="/dashboard">COMMENCER MAINTENANT</Link>
+          </Button>
+          <Image alt="Avatar running" height="100px" width="100px" src="/avatar.svg" />
+        </Stack>
+      </VStack>
+
+      <Box bgRepeat="no-repeat" bgAttachment="fixed" bgImage="url(./bandeau-home-vert.svg)" bgSize="cover" position="top">
+        <SimpleGrid p={4} minChildWidth="sm" gap="8">
+          <LinkBox>
+            <Card.Root size="lg" variant="elevated" border="1px" borderColor="gray.200">
+              <Card.Header>
+                <Icon fontSize="40px">
+                  <LinkOverlay href="/flashcards">
+                    <LuNotebookPen />
+                  </LinkOverlay>
+                </Icon>
+              </Card.Header>
+              <Card.Body gap={4}>
+                <Card.Title fontSize="3xl" color="orange.500">FlashCards</Card.Title>
+                <Text fontSize="xl">
+                  Retiens efficacement toutes les connaissances donc tu auras besoin pour les écrits !
+                </Text>
+              </Card.Body>
+            </Card.Root>
+          </LinkBox>
+
+          <LinkBox>
+            <Card.Root size="lg" variant="elevated" border="1px" borderColor="gray.200">
+              <Card.Header>
+                <Icon fontSize="40px">
+                  <LinkOverlay href="/notes">
+                    <FiBookOpen />
+                  </LinkOverlay>
+                </Icon>
+              </Card.Header>
+              <Card.Body gap={4}>
+                <Card.Title fontSize="3xl" color="orange.500">
+                  Fiches de révision
+                </Card.Title>
+                <Text fontSize="xl">
+                  Nous t'avons fait une synthèse de tous les grands thèmes que tu risques de rencontrer au concours.
+                </Text>
+              </Card.Body>
+            </Card.Root>
+          </LinkBox>
+
+          <LinkBox>
+            <Card.Root size="lg" variant="elevated" border="1px" borderColor="gray.200">
+              <Card.Header>
+                <Icon fontSize="40px">
+                  <LinkOverlay href="/quiz">
+                    <LuListTodo />
+                  </LinkOverlay>
+                </Icon>
+              </Card.Header>
+              <Card.Body gap={4}>
+                <Card.Title fontSize="3xl" color="orange.500">
+                  Quiz
+                </Card.Title>
+                <Text fontSize="xl">
+                  Après avoir lu une fiche, fais des quiz associés pour voir si tu as bien compris et assimilé le cours !
+                </Text>
+              </Card.Body>
+            </Card.Root>
+          </LinkBox>
+
+        </SimpleGrid>
+      </Box>
+    </Box>
   );
 }
