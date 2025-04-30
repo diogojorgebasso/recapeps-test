157de0081cccfb07d8ebc3d8591918da134b2742
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-04-30 19:12:36 +0200

    fix sitemap : map to actual recapeps.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,21 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://recapeps.fr',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: 'https://recapeps.fr/a-propos',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://acme.com/blog',
+      url: 'https://recapeps.fr/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
