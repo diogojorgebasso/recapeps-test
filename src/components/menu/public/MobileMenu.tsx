cb8d009a7dec704dd3c02d1d5c21f222b1f6c53d
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-05-08 10:58:34 +0200

    fix : remove mobile bar for anauth users.

--- src/components/menu/public/MobileMenu.tsx
+++ src/components/menu/public/MobileMenu.tsx
@@ -6,7 +6,14 @@ import { FaPencilRuler } from "react-icons/fa"; //ecrit 2
 import { RxChatBubble } from "react-icons/rx"; //oral 1
 import { SlSpeech } from "react-icons/sl"
 import { FaRegUserCircle } from "react-icons/fa";
-export default function MobileMenu() {
+import { getAuthenticatedAppForUser } from "@/lib/firebase/serverApp";
+
+export default async function MobileMenu() {
+    const { user } = await getAuthenticatedAppForUser();
+    if (!user) {
+        return null; // or redirect to login page
+    }
+
     return (
         <Box
             display={{ base: "block", md: "none" }}
@@ -14,31 +21,29 @@ export default function MobileMenu() {
             pos="fixed"
             bottom={0}
             insetX={0}
-            bg="bg.canvas"
-            borderTop="1px solid"
-            borderColor="border.default"
             zIndex="sticky"
         >
             <HStack justify="space-around" py={2}>
                 <Link href="/parcours/dashboard">
-                    <FaHome size={24} />
+                    <FaHome size={26} />
                 </Link>
                 <Link href="/parcours/ecrit-1">
-                    <FaPencilAlt size={24} />
+                    <FaPencilAlt size={26} />
                 </Link>
                 <Link href="/parcours/ecrit-2">
-                    <FaPencilRuler size={24} />
+                    <FaPencilRuler size={26} />
                 </Link>
                 <Link href="/parcours/oral-1">
-                    <RxChatBubble size={24} />
+                    <RxChatBubble size={26} />
                 </Link>
                 <Link href="/parcours/oral-3">
-                    <SlSpeech size={24} />
+                    <SlSpeech size={26} />
                 </Link>
                 <Link href="/compte/profil">
-                    <FaRegUserCircle size={24} />
+                    <FaRegUserCircle size={26} />
                 </Link>
             </HStack>
+
         </Box>
     );
 }
