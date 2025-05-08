215812e69a5c9d4050f29c867db0b48727187bc4
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-05-09 01:58:11 +0200

    feat: add favicon and update manifest icon reference

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,21 +1,22 @@
 import type { MetadataRoute } from 'next'
- 
+
 export default function manifest(): MetadataRoute.Manifest {
-return {
-    name: "Recapeps - Réussir ce n'est qu'une question d'entraînement",
-    short_name: 'Recapeps',
-    description: "Recap'eps est un outil de révision imparable pour réussir les concours du CAPEPS externe.",
-    start_url: '/',
-    scope: '/', // TODO : See if the scope is correct
-    display: 'standalone',
-    background_color: '#fff',
-    theme_color: '#ea580c',
-    icons: [
-        {
-            src: '/favicon.ico',
-            sizes: 'any',
-            type: 'image/x-icon',
-        },
-    ],
-}
+    return {
+        name: "Recapeps - Réussir ce n'est qu'une question d'entraînement",
+        short_name: 'Recapeps',
+        description: "Recap'eps est un outil de révision imparable pour réussir les concours du CAPEPS externe.",
+        start_url: '/',
+        scope: '/', // TODO : See if the scope is correct
+        display: 'standalone',
+        background_color: '#fff',
+        theme_color: '#ea580c',
+        icons: [
+            {
+                src: '/icon.ico',
+                sizes: 'any',
+                type: 'image/ico',
+                purpose: 'any',
+            },
+        ],
+    }
 }
