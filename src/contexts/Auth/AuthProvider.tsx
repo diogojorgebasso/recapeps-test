6f4f19ca170a5d56ed284e1b90672c13a6bf9b81
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-05-09 03:36:37 +0200

    WIP : Auth

--- src/contexts/Auth/AuthProvider.tsx
+++ src/contexts/Auth/AuthProvider.tsx
@@ -4,15 +4,12 @@ import { useState, useEffect } from 'react';
 import { AuthContext } from './useAuth';
 import { setCookie, deleteCookie } from "cookies-next";
 import { onIdTokenChanged } from '@/lib/firebase/auth';
-import { User as FirebaseAuthUser } from 'firebase/auth';
 import { ReactNode } from 'react';
-import { useRouter } from 'next/navigation';
 
 export function AuthProvider({ children, initialUser }: { children: ReactNode, initialUser: any }) {
     const [user, setUser] = useState(initialUser);
     const [pro, setPro] = useState<boolean>(false);
     const [loading, setLoading] = useState(!initialUser); // If we have initialUser, we're not loading
-    const router = useRouter();
 
     useEffect(() => {
         return onIdTokenChanged(async (user) => {
@@ -22,10 +19,13 @@ export function AuthProvider({ children, initialUser }: { children: ReactNode, i
             } else {
                 await deleteCookie("__session");
             }
-            if (initialUser?.uid !== user?.uid) router.refresh();
+            if (initialUser?.uid === user?.uid) {
+                return;
+            }
+            window.location.reload();
 
         });
-    }, [initialUser, router]);
+    }, [initialUser]);
 
     return (
         <AuthContext.Provider
