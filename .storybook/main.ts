c3bce4824a85b67e76a53ebd9d22d56bb8a34494
Diogo Basso <65865529+diogojorgebasso@users.noreply.github.com>
2025-05-31 17:38:12 +0200

    chore(storybook): update to the latest version.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import type { StorybookConfig } from "@storybook/nextjs-vite";
 
 const config: StorybookConfig = {
   "stories": [
@@ -6,13 +6,14 @@ const config: StorybookConfig = {
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
   ],
   "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
+    "name": "@storybook/nextjs-vite",
     "options": {}
   },
   "staticDirs": [
